8e10aa4a75118cc4e8a6c45dcb65d46509fe63ce
zohra-neda <zohra.neda@gmail.com>
2023-10-30 18:03:09 +0430

    Add and import reservationSlice in store.js file

--- src/Redux/reservations/reservationsSlice.js
+++ src/Redux/reservations/reservationsSlice.js
@@ -1,9 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+
 const reservationsSlice = createSlice({
     name: "reservations",
-    initialState: [],
-    reducers: [],
+    initialState: [
+        {
+            "start_date": "2023-10-30",
+            "end_date": "2023-12-28",
+            "user_id": 1,
+            "car_id": 1,
+            "city_id": 1
+        },
+        {
+            "start_date": "2023-10-30",
+            "end_date": "2023-12-28",
+            "user_id": 1,
+            "car_id": 1,
+            "city_id": 1
+        },{
+            "start_date": "2023-10-30",
+            "end_date": "2023-12-28",
+            "user_id": 1,
+            "car_id": 1,
+            "city_id": 1
+        }
+    ],
+    reducers: {
+        addReservation: (state, action) => {
+            state.push(action.payload);
+        }
+    },
 });
 
 export default reservationsSlice.reducer;
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import carsSlice from "./cars/carsSlice";
+import reservationsSlice from "./reservations/reservationsSlice";
 
 const store = configureStore({
     reducer: {
         cars: carsSlice,
+        reservationsSlice: reservationsSlice
     },
 })
 
