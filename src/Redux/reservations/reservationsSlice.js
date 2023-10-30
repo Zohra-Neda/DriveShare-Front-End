69b80cb8491329f7785d1f8e8be179861e4093c7
zohra-neda <zohra.neda@gmail.com>
2023-10-30 17:34:50 +0430

    Add reservationsSlice reducer

---
+++ src/Redux/reservations/reservationsSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const reservationsSlice = createSlice({
+    name: "reservations",
+    initialState: [],
+    reducers: [],
+});
+
+export default reservationsSlice.reducer;
