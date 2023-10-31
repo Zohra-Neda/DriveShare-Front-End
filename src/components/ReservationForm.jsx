43ce3b086f16d3b4b99f0ccac8ee7dc22a510ac9
zohra-neda <zohra.neda@gmail.com>
2023-10-31 22:05:33 +0430

    Update state path to access the cars and cities array

--- src/components/ReservationForm.jsx
+++ src/components/ReservationForm.jsx
@@ -1,60 +1,85 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux'
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { getReservations, postReservations } from '../Redux/reservations/reservationsSlice';
+import '../styles/reserve.css'
+
 
 function ReservationForm() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const cars = useSelector(state => state.cars.data); // Updated state path to access the cars array
+  const cities = useSelector(state => state.city.data); // Updated state path to access the cities array
 
-    useEffect(() => {
-        dispatch(getReservations());
-    }, [dispatch]); // Empty dependency array to ensure it runs once.
+  useEffect(() => {
+    dispatch(getReservations());
+  }, [dispatch]);
 
-  
+  const [formData, setFormData] = useState({
+    fullName: '',
+    car_id: '',
+    city: '',
+    start_date: '',
+    end_date: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(postReservations(name))
+    dispatch(postReservations(formData));
   }
-    
-    return (
-         <form
+
+  const { fullName, car_id, city, start_date, end_date } = formData;
+
+  return (
+    <main>
+      <div className="reservation-container">
+        <h1>Create New Reservation</h1>
+        <form
       id="reservation-form"
       className="d-flex flex-column align-items-center g-4"
+      onSubmit={handleSubmit}
     >
       <input
-        className="form-control mb-3"
+        className="form-control mb-3 mt-3"
         type="text"
         value={fullName}
-        disabled
+        onChange={handleChange}
+            name="fullName"
+            disabled
+        placeholder="Full Name" // Add a placeholder
       />
       <div className="row g-2 mb-3">
-        {!carSelected && (
-          <div className="col select-wrapper">
-            <select
-              className="form-select"
-              value={formData.service_id}
-              onChange={handleChange}
-              name="service_id"
-            >
-              <option value="">Select a service</option>
-              {cars.map((car) => (
-                <option key={car.id} value={car.id}>
-                  {car.name}
-                </option>
-              ))}
-            </select>
-          </div>
-        )}
         <div className="col select-wrapper">
           <select
             className="form-select"
-            value={formData.city}
+            value={car_id}
+            onChange={handleChange}
+            name="car_id"
+          >
+            <option value="">Select a car</option>
+            {cars?.map((car) => (
+              <option key={car.id} value={car.id}>
+                {car.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="col select-wrapper">
+          <select
+            className="form-select"
+            value={city}
             onChange={handleChange}
             name="city"
           >
-            <option value="">Select a location</option>
-            {locations.map((location) => (
-              <option key={location.id} value={location.name}>
-                {location.name}
+            <option value="">Select a city</option>
+            {cities?.map((city) => (
+              <option key={city.id} value={city.name}>
+                {city.name}
               </option>
             ))}
           </select>
@@ -63,15 +88,24 @@ function ReservationForm() {
       <input
         className="form-control mb-3"
         type="date"
-        value={formData.date}
+        value={start_date}
         onChange={handleChange}
-        name="date"
+        name="start_date"
       />
-      <button type="submit" onClick={handleSubmit} className="btn btn-light text-success">
+      <input
+        className="form-control mb-3"
+        type="date"
+        value={end_date}
+        onChange={handleChange}
+        name="end_date"
+      />
+      <button type="submit" className="btn btn-light text-success">
         Create Reservation
       </button>
     </form>
-    );
+      </div>
+    </main>
+  );
 }
 
 export default ReservationForm;
