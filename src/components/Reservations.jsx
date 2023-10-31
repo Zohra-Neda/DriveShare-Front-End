797433b88eb61a3ac535bee74d63a6983ddc9f64
zohra-neda <zohra.neda@gmail.com>
2023-10-31 16:14:57 +0430

    Create ReservationsCard component

--- src/components/Reservations.jsx
+++ src/components/Reservations.jsx
@@ -1,44 +1,30 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { getReservations } from '../Redux/reservations/reservationsSlice';
+import ReservationsCard from './ReservationsCard';
+
 
 function Reservations() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getReservations());
-    }, [dispatch]); // Empty dependency array to ensure it runs once.
+  useEffect(() => {
+    dispatch(getReservations());
+  }, [dispatch]);
 
-    const reservations = useSelector(state => state.reservationsSlice);
-    
-    return (
-        <section className="res-container">
-            <div><h1>My Reservations</h1></div>
-            <div className="res-details">
+  const reservations = useSelector(state => state.reservationsSlice.data);
+  console.log(reservations);
+
+
+  return (
+    <section className="res-container">
+      <div><h1>My Reservations</h1></div>
+      <div className="res-details">
         {reservations.map((reservation) => (
-          <div key={reservation.id}>
-            <div className="reservation">
-              <div className="res-texts">
-                <h5>{reservation.car_id}</h5>
-                <p>
-                  City:
-                  {reservation.city_id}
-                </p>
-                <p>
-                  Star_Date:
-                  {reservation.start_date}
-                        </p>
-                        <p>
-                  End_Date:
-                  {reservation.end_date}
-                </p>
-              </div>
-            </div>
-          </div>
+          <ReservationsCard reservation={reservation} key={reservation.id}/>
         ))}
       </div>
-        </section>
-    );
+    </section>
+  );
 }
 
 export default Reservations;
