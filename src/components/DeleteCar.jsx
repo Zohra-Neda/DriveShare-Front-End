5c111fe98dfb6d0893b391ade792cc256e98dc8e
Aamir <aamirasaram2107@gmail.com>
2023-11-01 14:56:55 +0200

    Add basic responsiveness to delete page

--- src/components/DeleteCar.jsx
+++ src/components/DeleteCar.jsx
@@ -12,8 +12,8 @@ const DeleteCar = () => {
   console.log(cars);
 
   return (
-    <div className="ml-[20%] flex flex-col flex-wrap h-screen overflow-scroll">
-      <h1 className="fixed top-4">Delete Car</h1>
+    <div className="lg:w-4/5 lg:ml-[20%] flex flex-col flex-wrap lg:h-screen overflow-scroll">
+      <h1 className="bg-white sm:w-full py-4 fixed top-0">Delete Car</h1>
       <div className="flex flex-col flex-wrap">
         {cars.map((car) => (
           <div key={car.id} className="p-4 my-4 border border-black flex justify-between">
